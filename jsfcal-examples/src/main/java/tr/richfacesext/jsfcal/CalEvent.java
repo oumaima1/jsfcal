69ad0cafeda82904b83da597cd94bb118a322da6
mcaliskan <mcaliskan@f427a04e-5e93-11de-8c10-5fe6993ea61f>
2009-06-22 12:53:36 +0000

    initial

---
+++ jsfcal-examples/src/main/java/tr/richfacesext/jsfcal/CalEvent.java
@@ -0,0 +1,58 @@
+package tr.richfacesext.jsfcal;
+
+import java.util.Date;
+
+import tr.richfacesext.components.jsfcal.Event;
+
+/**
+ * 
+ * @author mert
+ *
+ */
+public class CalEvent implements Event {
+
+	private Long id;
+	private String title;
+	private Date startDate;
+	private Date endDate;
+	private boolean isReadOnly;
+	
+	public CalEvent(int id, String title, Date start, Date end, boolean readOnly) {
+		this.id = new Long(id);
+		this.title = title;
+		this.startDate = start;
+		this.endDate = end;
+		this.isReadOnly = readOnly;
+	}
+	
+	public Long getEventId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public boolean isReadOnly() {
+		return isReadOnly;
+	}
+	public void setReadOnly(boolean isReadOnly) {
+		this.isReadOnly = isReadOnly;
+	}
+}
